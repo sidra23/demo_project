1f1500ee31aa81143b219a3d750d85f1fda852e3
sidra23 <sidraghumman728@gmail.com>
2025-01-31 07:37:42 -0800

    Initial commit with Terraform and GitHub Actions setup

---
+++ main.tf
@@ -0,0 +1,28 @@
+# main.tf
+
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
+
+variable "github_token" {
+  description = "GitHub personal access token"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_owner" {
+  description = "GitHub owner or organization"
+  type        = string
+}
+
+resource "github_repository" "example" {
+  name        = "example-repo"
+  description = "This is an example repository created by Terraform"
+  visibility  = "public"
+  auto_init   = true
+}
+
+output "repository_url" {
+  value = github_repository.example.html_url
+}
